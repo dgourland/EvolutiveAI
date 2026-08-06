a67e1379e2fadef6e066a6b0da0b1b0115d12313
Gourland Dorian <dorian.gourland@equasens.com>
2026-08-06 11:10:16 +0200

    Début de la création du launcheur

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo apt -y install python3.13-venv
 python3 -m venv .
 ./bin/pip install numba pygame-ce pygame scipy PyOpenGL PyOpenGL_accelerate 
-mkdir -p saves/preys
-mkdir -p saves/predator
+mkdir -p saves/default/preys
+mkdir -p saves/default/predator
 mkdir logs
